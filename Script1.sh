4c044d3d2531a7b0870dcfc712baea823bdb0204
Pankajkade <72438315+Pankajkade@users.noreply.github.com>
2023-09-30 16:45:01 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-#This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
+echo "Learn Hadoop"
+echo "Learn Sqoop"
+echo "Learn Kafka"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-20, We are learning Jenkins"
